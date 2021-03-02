518ef6721a40ab7d2efa4a74b750d12440811543
周爱桩 <zhouaizhuang000@163.com>
2021-03-02 15:26:06 +0800

    [ADD]添加策略模式进行表单校验

---
+++ formCheck/check.js
@@ -0,0 +1,54 @@
+// 校验必填
+const isRequire = function (val, msg) {
+  if(!val) { return msg }
+}
+// 校验手机
+const isPhone = function (val, msg) {
+  if(!/^1[3456789]\d{9}$/.test(val)) { return msg }
+}
+// 字符串最小长度
+const minLength = function (val, len, msg) {
+  if(val.length < len) { return msg }
+}
+// 字符串最大长度
+const maxLength = function (val, len, msg) {
+  if(val.length > len) { return msg }
+}
+const check = {
+  isRequire, isPhone, minLength, maxLength
+}
+
+// 表单校验实例
+const Validator = function () {
+  this.cache = [] // 保存校验规则
+}
+// 添加校验函数
+Validator.prototype.add = function(val, rule, msg) {
+  this.cache.push(function() {
+    let [func, ...arg] = rule.split(':')
+    arg.push(msg)
+    return check[func](val, ...arg)
+  })
+}
+// 开始校验
+Validator.prototype.start = function() {
+  for ( var i = 0, validatorFunc; validatorFunc = this.cache[ i++ ];){
+    var msg = validatorFunc() // 开始校验，并取得校验后的返回信息
+    if (msg) { // 如果有确切的返回值，说明校验没有通过
+      return msg
+    }
+  }
+}
+/**使用方法 */
+function checkForm(){
+  const [userName, password, phoneNumber] = ['asasd', 'asdsassad', '15388178152']
+  var validator = new Validator() // 创建一个 validator 对象
+  validator.add(userName, 'isRequire', '用户名不能为空')
+  validator.add(userName, 'minLength:5', '用户名长度不能少于5位') // 通过冒号进行额外参数的传递
+  validator.add(password, 'minLength:10', '密码长度不能少于6位')
+  validator.add(phoneNumber, 'isPhone', '请输入正确的手机号码')
+  var errorMsg = validator.start() // 获得校验结果
+  console.log(errorMsg)
+  return errorMsg
+}
+checkForm()
