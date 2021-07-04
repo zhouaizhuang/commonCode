e4c699f84f6b6d36c1d87c211f1c8db2d19bf063
周爱桩 <zhouaizhuang000@163.com>
2021-07-04 09:40:07 +0800

    引入公共样式

--- Vue/Vue项目架构/src/main.js
+++ Vue/Vue项目架构/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import reg from "@/components/common/reg"
+import "./common.css"
 Vue.use(reg)
 Vue.config.productionTip = false
 
