3a5e9d8e67377aec0103d48f848b8ad74e17a557
周爱桩 <zhouaizhuang000@163.com>
2021-08-13 10:16:59 +0800

    添加注释

--- Vue/注册Toast高频组件/component/hot/register.js
+++ Vue/注册Toast高频组件/component/hot/register.js
@@ -10,7 +10,7 @@ export default {
     const toast = new toastContrustor() // 2.new，根据组件构造器，创建一个组件对象
     toast.$mount(document.createElement('div')) // 3.将组件对象挂载到元素上
     document.body.appendChild(toast.$el) // 4.toast.$el对应的是div
-    Vue.prototype.$toast = toast
+    Vue.prototype.$toast = toast  // 这里定义之后，那么以后想使用toast组件只需要这样使用：   this.$toast('申请成功', 2000)
     // 后面还可以继续注册其他的高频组件....
     
   }
