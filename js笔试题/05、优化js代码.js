02b64410933a609adf729429153c23fc790b25a6
周爱桩 <840043348@qq.com>
2021-10-26 23:31:21 +0800

    优化文案

--- js笔试题/05、优化js代码.js
+++ js笔试题/05、优化js代码.js
@@ -1,11 +1,11 @@
 /**
- * 1、在Vue项目中，有一个实习生写的函数是这样的，调用/xxx/list接口获取数据后，将数据处理后，进行赋值。并渲染到页面上
+ * 1、在Vue项目中，有一个实习生写的函数是这样的，调用/userInfo/list接口获取数据后，将数据处理后，进行赋值。并渲染到页面上
  * 希望你能在不改变原有功能的情况下，优化这段代码。
  * 预期：优化到你认为合适的版本。
  */
 async function getList(){
   this.spinning = true // 显示loading
-  var res = await post('/xxx/list') // post请求请求到数据。
+  var res = await post('/userInfo/list') // post请求请求到数据。
   var list = res.data.list // 用于渲染的数据列表
   this.total = res.data.total // 总页数
   if(list.length > 0) {
