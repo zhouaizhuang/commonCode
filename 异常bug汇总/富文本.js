4b06c7a657ad7cccea3c13c9aa5ba2bbc5148ca2
周爱桩 <840043348@qq.com>
2022-03-10 10:10:53 +0800

    增加注释

--- 异常bug汇总/富文本.js
+++ 异常bug汇总/富文本.js
@@ -1,4 +1,4 @@
-// 1、报错：webpackJsonp is not defined？什么情况
+// 1、引用vue-ueditor-wrap富文本插件后，打包上线就报错：webpackJsonp is not defined？什么情况
 // 解决： https://zhuanlan.zhihu.com/p/47096548
 // 这是因为公共文件必须在自己引用的js文件之前引用，可以手动改文件引用。找到build→webpack.prod.conf.js→找到HtmlWebpackPlugin插件，添加如下配置：
 new HtmlWebpackPlugin({
@@ -15,4 +15,4 @@ new HtmlWebpackPlugin({
   chunks: ['manifest', 'vendor', 'app'], // 注意！！！！！！！！！手动添加这一行代码！！！！！！！！
   // necessary to consistently work with multiple chunks via CommonsChunkPlugin
   chunksSortMode: 'dependency'
-}) 
+})
