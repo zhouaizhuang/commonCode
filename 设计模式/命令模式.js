4c6a9eca49a01c92e1f8a9c55ad1c802bd714858
周爱桩 <840043348@qq.com>
2022-10-14 20:33:30 +0800

    命令模式深入理解

--- 设计模式/命令模式.js
+++ 设计模式/命令模式.js
@@ -1,44 +1,36 @@
-const ruy = {
-  attack: function (){console.log('攻击')},
-  defense: function (){console.log('防御')},
-  jump: function (){console.log('跳跃')},
-  crouch: function (){console.log('蹲下')},
-}
-const makeCommand = function (receiver, state) {
-  return function (){ receiver[state]() }
-}
-var commands = {'119': 'jump','115': 'crouch','97': 'defense','100': 'attack'}
-var commandStack= []
-document.onKeypress = function (e) {
-  var keyCode = e.keyCode
-  var command = ruy[commands[keyCode]]
-  if(command){
-    command(); // 执行命令
-    commandStack.push(command) // 将执行的命令保存到堆栈
+// 出拳
+const fist = {
+  execute(){
+    console.log('出拳')
   }
 }
-document.getElementById('repay').onclick = function () { // 播放录像
-  var command = null
-  while(command = commandStack.shift()){ command() } // 从堆栈中依次去除命令并执行
+// 跳跃
+const jump = {
+  execute(){
+    console.log('跳跃')
+  }
 }
-
-
-
-// =============宏命令===========
-var closeDoorCommand = { execute: function () {console.log('关门')} }
-var openPcCommand = { execute: function () {console.log('开电脑')} }
-var openQQCommand = { execute: function () {console.log('登陆QQ')} }
-var MacroCommand = function () {
+// 踢
+const leg = {
+  execute(){
+    console.log('踢')
+  }
+}
+// 玩游戏
+const playGame = function (){
   return {
-    commandsList: [],
-    add: function(command){this.commandsList.push(command)},
+    command: [],
+    add: function (val){
+      this.command.push(val)
+    },
     execute: function (){
-      this.commandsList.forEach(item => item.execute())
+      this.command.forEach(item => item.execute())
     }
   }
 }
-var macroCommand = MacroCommand()
-macroCommand.add(closeDoorCommand)
-macroCommand.add(openPcCommand)
-macroCommand.add(openQQCommand)
-macroCommand.execute()
+// 我的游戏
+const myGame = playGame()
+myGame.add(fist)
+myGame.add(jump)
+myGame.add(leg)
+myGame.execute()
