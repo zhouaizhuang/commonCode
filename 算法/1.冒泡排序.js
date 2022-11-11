0b9efb26507508421c5ca5f259f0199fccd6b23d
周爱桩 <840043348@qq.com>
2022-11-11 10:27:04 +0800

    新增冒泡排序

---
+++ 算法/1.冒泡排序.js
@@ -0,0 +1,11 @@
+function BubblingSort(arr){
+  for(let i = 0, len = arr.length; i < len; i++) {
+    for(let j = 0; j < len - i - 1; j++) {
+      if(arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      }
+    }
+  }
+  return arr
+}
+console.log(BubblingSort([6,2,1,5,2,8,23,8,39,32,19,54]))
