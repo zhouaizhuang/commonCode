1b3567ee6b5fe34012e07364e2e955b3510da033
周爱桩 <840043348@qq.com>
2022-11-14 15:41:14 +0800

    格式化代码

--- 算法/1.桶排序.js
+++ 算法/1.桶排序.js
@@ -1,5 +1,5 @@
-function bucketSort(arr){
+function bucketSort(arr) {
   let res = arr.reduce((prev, item) => (prev[item]++ || (prev[item] = 1), prev), {})
   return Object.keys(res).reduce((prev, item) => [...prev, ...new Array(Number(res[item])).fill(Number(item))], [])
 }
-console.log(bucketSort([1,8,3,6,3,5,8,3,6,2,9,0,54]))
+console.log(bucketSort([1, 8, 3, 6, 3, 5, 8, 3, 6, 2, 9, 0, 54]))
